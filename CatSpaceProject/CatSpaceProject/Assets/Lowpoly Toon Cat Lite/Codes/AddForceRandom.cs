7daef2ff6e2e6b80ec963a6f024bd016a76c30e0
mdechdee <dechdee.m@gmail.com>
2018-11-11 15:08:20 +0900

    Add codes&cats properties
    Cats
    - Add rigidbody
    - Add Capsule Collider
    
    Codes
    - Add random forces code
    
    Animation
    - Slower the cat floating animation
    
    Etc.
    - Delete default animation included in cat_lite
    - Moves Animation into animation folder

---
+++ CatSpaceProject/CatSpaceProject/Assets/Lowpoly Toon Cat Lite/Codes/AddForceRandom.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddForceRandom : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Rigidbody[] allObjects = FindObjectsOfType(typeof(Rigidbody)) as Rigidbody[];
+        foreach(Rigidbody obj in allObjects)
+        {
+            obj.AddTorque(Random.Range(0, 5), Random.Range(0, 5), Random.Range(0, 5));
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
