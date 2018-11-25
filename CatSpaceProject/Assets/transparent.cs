54d7970b4ac1326c10a1c0a906fe535931a1a97c
PWonjung <fabiola@kaist.ac.kr>
2018-11-25 13:36:46 +0900

    two constellation added

---
+++ CatSpaceProject/Assets/transparent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class transparent : MonoBehaviour {
+    RawImage m_Image;
+
+	// Use this for initialization
+	void Start () {
+        m_Image = GetComponent<RawImage>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float alpha = 0.5f; //1 is opaque, 0 is transparent
+        Color currColor = m_Image.color;
+        currColor.a = alpha;
+        m_Image.color = currColor;
+	}
+}
