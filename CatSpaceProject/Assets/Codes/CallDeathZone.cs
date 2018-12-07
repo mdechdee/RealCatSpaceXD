81de06feb07b7228db7b01a367c434177d0951b6
mdechdee <dechdee.m@gmail.com>
2018-12-08 02:16:45 +0900

    [Add] DeathZone is implemented
    Death Zone is determined by a object which have the border in its childs.
    The DeathZoneScript is to countdown the time that player left the zone and restart the scene if the time exceeds.
    The CallDeathZone script must be attach to the death zone child to activate deathzone.
    - Demo "DeathZone" scene is added.
    - Add flashing text animation.
    - The gui added to the existed 'HUDcanvas' canvas.

---
+++ CatSpaceProject/Assets/Codes/CallDeathZone.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CallDeathZone : MonoBehaviour {
+
+    // Use this for initialization
+    private void OnTriggerEnter(Collider collider)
+    {
+        print(1);
+        gameObject.GetComponentInParent<DeathZoneScript>().Call();
+        
+    }
+}
