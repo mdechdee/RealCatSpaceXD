70d45ab2c7a3652a260227a675ac173cfe1f018a
PWonjung <fabiola@kaist.ac.kr>
2018-12-08 19:36:35 +0900

    Chg Envir

---
+++ CatSpaceProject/Assets/Codes/hint.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class hint : MonoBehaviour {
+    public string hint_name;
+    GameObject hint_obj;
+    float hint_time;
+    bool count;
+	// Use this for initialization
+
+    void Start () {
+        hint_obj = GameObject.Find("HUDCanvas/" + hint_name);
+        hint_time = -1.0f;
+
+	}
+	void OnCollisionEnter (Collision col)
+    {
+        if(col.gameObject.name == "Cat Lite")
+        {
+            hint_obj.GetComponent<RawImage>().enabled = true;
+            this.GetComponent<BoxCollider>().enabled = false;
+            hint_time = 0.0f;   
+        }
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+        if (hint_time >= 0.0f) {
+            hint_time += Time.deltaTime;
+        }
+        if (hint_time > 5.0f) {
+            hint_obj.GetComponent<RawImage>().enabled = false;
+            hint_time = -1.0f;
+        }
+	}
+}
---
+++ CatSpaceProject/Assets/start_hint.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class start_hint : MonoBehaviour {
+
+    IEnumerator Example()
+    {
+        yield return new WaitForSeconds(5);
+        this.GetComponent<RawImage>().enabled = false;
+    }
+
+
+	// Use this for initialization
+	void Start () {
+       this.GetComponent<RawImage>().enabled = true;
+        StartCoroutine(Example());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
