c34bdb52fbe242fe260bda5dc3f6a514c57f08e3
JunghooHwang <41410939+JunghooHwang@users.noreply.github.com>
2018-12-09 00:38:57 +0900

    [Update] Inventory
    :-)

--- CatSpaceProject/Assets/Codes/CatMovement.cs
+++ CatSpaceProject/Assets/Codes/CatMovement.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CatMovement : MonoBehaviour {
 
@@ -12,11 +13,15 @@ public class CatMovement : MonoBehaviour {
 
     public float startfuelLevel = 100;
     public float fuelLevel;
+    Color orange;
+    Color nocolor;
 
+    GameObject itemparent;
+    int throw_inven;
     Rigidbody catbody;
     Collider catcollider;
-
     GameObject[] items;
+    GameObject[] throw_items;
     public GameObject obj;
     Rigidbody objbody;
 
@@ -28,7 +33,7 @@ public class CatMovement : MonoBehaviour {
 
     bool throwObjects;
     public bool useJetpack;
-
+    Transform[] childitem;
     GameObject player;
     GameObject playerEquipPoint;
     public bool isPicking;
@@ -45,6 +50,17 @@ public class CatMovement : MonoBehaviour {
 
     void Start()
     {
+        itemparent = GameObject.Find("Item");
+        throw_items = new GameObject[5];
+        throw_inven = 1;
+        orange = new Color(0.9f, 0.5f, 0.1f, 1.0f);
+        nocolor = new Color(0.0f, 0.0f, 0.0f, 0.0f);
+        for (int i = 1; i < 6; i++)
+        {
+            GameObject slots = GameObject.Find("HUDCanvas/InventoryPanel/Slot_" + i.ToString());
+            slots.GetComponent<Image>().color = nocolor;
+        }
+
 
     }
 
@@ -56,9 +72,19 @@ public class CatMovement : MonoBehaviour {
 
         if (Input.GetKey(KeyCode.LeftShift) && fuelLevel > 0 && (hmove != 0 || vmove != 0))
             useJetpack = true;
-        if (isPicking == true && Input.GetKeyDown(KeyCode.C))
+        if (Input.GetKeyDown(KeyCode.C))
             throwObjects = true;
-        
+        if (Input.GetKeyDown(KeyCode.I)) {
+
+            GameObject oldborder = GameObject.Find("HUDCanvas/InventoryPanel/Slot_" + throw_inven.ToString());
+            oldborder.GetComponent<Image>().color = nocolor;
+            throw_inven =(throw_inven+1)%5;
+            if (throw_inven == 0)
+                throw_inven = 5;
+            GameObject border = GameObject.Find("HUDCanvas/InventoryPanel/Slot_"+throw_inven.ToString());
+            border.GetComponent<Image>().color = orange;
+        }
+
     }
 
     void FixedUpdate()
@@ -99,11 +125,69 @@ public class CatMovement : MonoBehaviour {
         useJetpack = false;
 
     }
+    void adjustText() {
+        int numitem = 0;
+
+
+        throw_items = new GameObject[5];
+        childitem = playerEquipPoint.GetComponentsInChildren<Transform>();
+        for (int i =1; i < 6; i++) {
+            GameObject panel = GameObject.Find("HUDCanvas/InventoryPanel/Slot_" + i.ToString() + "/Border/ItemImage");
+            panel.GetComponent<Text>().text = "";
+        }
+        int countitem = 0;
+        foreach (Transform item in childitem)
+        {
+            if (item.gameObject.tag == "Item")
+            {
+                throw_items[countitem] = item.gameObject;
+                GameObject panel = GameObject.Find("HUDCanvas/InventoryPanel/Slot_" + (countitem + 1).ToString() + "/Border/ItemImage");
+                Debug.Log(panel.GetComponent<Text>().text);
+                panel.GetComponent<Text>().text = item.gameObject.GetComponent<Rigidbody>().mass.ToString() + "kg";
+                countitem++;
+            }
+
+        }
+        foreach (Transform item in childitem)
+        {
+            if (item.gameObject.tag == "Item")
+            {
+                numitem += 1;
+            }
+
+        }
+        for (int i = 1; i < 6; i++)
+        {
+            GameObject slots = GameObject.Find("HUDCanvas/InventoryPanel/Slot_" + i.ToString());
+            slots.GetComponent<Image>().color = nocolor;
+        }
+
+        if (numitem > 0)
+        {
+            GameObject border = GameObject.Find("HUDCanvas/InventoryPanel/Slot_1");
+            border.GetComponent<Image>().color = orange;
+            throw_inven = 1;
+        }
+
+
+    }
 
     void ThrowObjects()
     {
         if (!throwObjects)
             return;
+
+        childitem = playerEquipPoint.GetComponentsInChildren<Transform>();
+        int countitem = 0;
+        foreach (Transform item in childitem)
+        {
+            if (item.gameObject.tag=="Item") {
+                throw_items[countitem] = item.gameObject;
+                countitem++;
+            }
+
+        }
+        objbody = throw_items[throw_inven-1].GetComponent<Rigidbody>();
 
         throwingPower = objbody.mass * 2; // F = dp/dt = d(mv)/dt
         movement.Set(hmove, 0, vmove);
@@ -111,7 +195,10 @@ public class CatMovement : MonoBehaviour {
         objbody.AddForce((-1) * movement.normalized * throwingPower, ForceMode.Impulse);
 
         SetEquip(obj, false);
-        playerEquipPoint.transform.DetachChildren();
+        throw_items[throw_inven - 1].GetComponent<Collider>().enabled = true;
+        throw_items[throw_inven - 1].transform.SetParent(itemparent.transform);
+      
+        adjustText();
         isPicking = false;
 
         throwObjects = false;
--- CatSpaceProject/Assets/Codes/itemScript.cs
+++ CatSpaceProject/Assets/Codes/itemScript.cs
@@ -10,7 +10,7 @@ public class itemScript : MonoBehaviour {
     CatMovement playerLogic;
 
     Collider[] itemcolliders;
-
+    Transform[] childitem;
     Vector3 forceDirection;
     public bool isPlayerEnter;
 
@@ -18,7 +18,7 @@ public class itemScript : MonoBehaviour {
     float flashSpeed = 5f;
     Color flashColor = Color.white;
     Transform itemtransform;
-
+    Color orange,nocolor;
     void Awake()
     {
         itemcolliders = GetComponentsInChildren<Collider>();
@@ -34,19 +34,58 @@ public class itemScript : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-		
-	}
-	
+        orange = new Color(0.9f, 0.5f, 0.1f, 1.0f);
+        nocolor = new Color(0.0f, 0.0f, 0.0f, 0.0f);
+
+    }
+    void turn_slot1() {
+        for (int i = 1; i < 6; i++)
+        {
+            GameObject slots = GameObject.Find("HUDCanvas/InventoryPanel/Slot_" + i.ToString());
+            slots.GetComponent<Image>().color = nocolor;
+        }
+
+
+        GameObject border = GameObject.Find("HUDCanvas/InventoryPanel/Slot_1");
+        border.GetComponent<Image>().color = orange;
+
+    }
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.X) && isPlayerEnter)
         {
-            transform.SetParent(playerEquipPoint.transform);
-            transform.localPosition = Vector3.zero;
-            transform.rotation = new Quaternion(0, 0, 0, 0);
 
-            playerLogic.Pickup(gameObject);
-            isPlayerEnter = false;
+
+            childitem = playerEquipPoint.GetComponentsInChildren<Transform>();
+            int countitem=0;
+            foreach (Transform item in childitem)
+            {
+                if (item.gameObject.tag == "Item") {
+                    countitem += 1;
+                }
+            }
+            Debug.Log(countitem);
+            if (countitem < 5)
+            {
+                transform.SetParent(playerEquipPoint.transform);
+                transform.localPosition = Vector3.zero;
+                transform.rotation = new Quaternion(0, 0, 0, 0);
+                this.GetComponent<Collider>().enabled = false;
+
+                playerLogic.Pickup(gameObject);
+                isPlayerEnter = false;
+                GameObject panel = GameObject.Find("HUDCanvas/InventoryPanel/Slot_" + (countitem + 1).ToString()+"/Border/ItemImage");
+                Debug.Log(panel.GetComponent<Text>().text);
+                panel.GetComponent<Text>().text = this.GetComponent<Rigidbody>().mass.ToString()+"kg";
+                if (countitem == 0) {
+                    turn_slot1();
+                }
+
+            }
+            else {
+
+            }
+            
             
         }
 
@@ -60,12 +99,26 @@ public class itemScript : MonoBehaviour {
     }
 
     void OnTriggerEnter(Collider other)
-    { 
-        if (other.gameObject == player)
+    {
+
+        childitem = playerEquipPoint.GetComponentsInChildren<Transform>();
+        int countitem = 0;
+        foreach (Transform item in childitem)
         {
-            isPlayerEnter = true;
-            ItemText.color = flashColor;
-        }     
+            if (item.gameObject.tag == "Item")
+            {
+                countitem += 1;
+            }
+        }
+        if (countitem < 5)
+        {
+            if (other.gameObject == player)
+            {
+                isPlayerEnter = true;
+                ItemText.color = flashColor;
+            }
+        }
+
 
     }
 
